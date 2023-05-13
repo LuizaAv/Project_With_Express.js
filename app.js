01f1b1c447a0c68bfb4fe3a35e75e658b23bf307
LuizaAv <l.avetisyan7777@gmail.com>
2023-05-13 23:51:25 +0400

    pushing into github already done project

---
+++ app.js
@@ -0,0 +1,33 @@
+const { Configuration, OpenAIApi } = require("openai");
+require('dotenv').config()
+
+const express = require('express');
+const app = express();
+
+const PORT = 4000
+
+app.get("/question", (req, res) => {
+  const message = req.query.message
+  res.send(message)
+
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+  
+  async function askAquestionCHATGPT(text){
+      try{
+          const completion = await openai.createCompletion({
+              model: "text-davinci-003",
+              prompt: text,
+              max_tokens: 2000
+            });
+          console.log(completion.data.choices[0].text);
+      }catch(error){
+          console.log(error)
+      }
+  }
+
+  askAquestionCHATGPT(message)
+})
+
